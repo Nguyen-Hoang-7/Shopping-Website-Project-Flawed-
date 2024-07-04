8a8694673519e4329102c782ad1fd39d69e73dfb
Nguyen-Hoang-7 <21522100@gm.uit.edu.vn>
2024-07-05 01:04:02 +0700

    create a website using django python

---
+++ ecommerce/app/models.py
@@ -0,0 +1,131 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+CATEGORY_CHOICES = (
+    ('CR', 'Curd'),
+    ('ML', 'Milk'),
+    ('LS', 'Lassi'),
+    ('MS', 'Milkshake'),
+    ('PN', 'Paneer'),
+    ('GH', 'Ghee'),
+    ('CZ', 'Cheese'),
+    ('IC', 'Ice-Creams'),
+)
+
+STATE_CHOICES = (
+    ('Alabama', 'Alabama'),
+    ('Alaska', 'Alaska'),
+    ('Arizona', 'Arizona'),
+    ('Arkansas', 'Arkansas'),
+    ('California', 'California'),
+    ('Colorado', 'Colorado'),
+    ('Connecticut', 'Connecticut'),
+    ('Delaware', 'Delaware'),
+    ('Florida', 'Florida'),
+    ('Georgia', 'Georgia'),
+    ('Hawaii', 'Hawaii'),
+    ('Idaho', 'Idaho'),
+    ('Illinois', 'Illinois'),
+    ('Indiana', 'Indiana'),
+    ('Iowa', 'Iowa'),
+    ('Kansas', 'Kansas'),
+    ('Kentucky', 'Kentucky'),
+    ('Louisiana', 'Louisiana'),
+    ('Maine', 'Maine'),
+    ('Maryland', 'Maryland'),
+    ('Massachusetts', 'Massachusetts'),
+    ('Michigan', 'Michigan'),
+    ('Minnesota', 'Minnesota'),
+    ('Mississippi', 'Mississippi'),
+    ('Missouri', 'Missouri'),
+    ('Montana', 'Montana'),
+    ('Nebraska', 'Nebraska'),
+    ('Nevada', 'Nevada'),
+    ('New Hampshire', 'New Hampshire'),
+    ('New Jersey', 'New Jersey'),
+    ('New Mexico', 'New Mexico'),
+    ('New York', 'New York'),
+    ('North Carolina', 'North Carolina'),
+    ('North Dakota', 'North Dakota'),
+    ('Ohio', 'Ohio'),
+    ('Oklahoma', 'Oklahoma'),
+    ('Oregon', 'Oregon'),
+    ('Pennsylvania', 'Pennsylvania'),
+    ('Rhode Island', 'Rhode Island'),
+    ('South Carolina', 'South Carolina'),
+    ('South Dakota', 'South Dakota'),
+    ('Tennessee', 'Tennessee'),
+    ('Texas', 'Texas'),
+    ('Utah', 'Utah'),
+    ('Vermont', 'Vermont'),
+    ('Virginia', 'Virginia'),
+    ('Washington', 'Washington'),
+    ('West Virginia', 'West Virginia'),
+    ('Wisconsin', 'Wisconsin'),
+    ('Wyoming', 'Wyoming'),
+)
+
+STATUS_CHOICES = (
+    ('Accepted', 'Accepted'),
+    ('Packed', 'Packed'),
+    ('On The Way', 'On The Way'),
+    ('Delivered', 'Delivered'),
+    ('Cancel', 'Cancel'),
+    ('Pending', 'Pending'),
+)
+
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    selling_price = models.FloatField()
+    discounted_price = models.FloatField()
+    description = models.TextField()
+    composition = models.TextField(default='')
+    prodapp = models.TextField(default='')
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
+    product_image = models.ImageField(upload_to='product')
+    def __str__(self):
+        return self.title
+    
+class Customer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    locality = models.CharField(max_length=200)
+    city = models.CharField(max_length=50)
+    mobile = models.IntegerField(default=0)
+    zipcode = models.IntegerField()
+    state = models.CharField(choices=STATE_CHOICES, max_length=100)
+    def __str__(self):
+        return self.name
+    
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    @property
+    def total_cost(self):
+        return self.quantity * self.product.discounted_price
+    
+class Payment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.FloatField()
+    razorpay_order_id = models.CharField(max_length=100, blank=True, null=True)
+    razorpay_payment_status = models.CharField(max_length=100, blank=True, null=True)
+    razorpay_payment_id = models.CharField(max_length=100, blank=True, null=True)
+    paid = models.BooleanField(default=False)
+
+class OrderPlaced(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    ordered_date = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=50, choices=STATUS_CHOICES, default='Pending')
+    payment = models.ForeignKey(Payment, on_delete=models.CASCADE, default="")
+    @property
+    def total_cost(self):
+        return self.quantity * self.product.discounted_price
+    
+class Wishlist(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
